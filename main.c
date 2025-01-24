3b8fde4350b1725823239ef8c7e937483a77ab0d
fardin.mahid <fardin.mahid@ucalgary.ca>
2025-01-24 07:30:10 -0700

    initial commit of project

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Fardin Mahid
+ *
+ * Created on January 24, 2025, 7:10 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
